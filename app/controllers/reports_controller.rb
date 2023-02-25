1c03ce9c34ceafb68123c7b7c48babf49b0b4d08
MaxRetana <maxretana981@gmail.com>
2023-02-24 18:19:51 -0600

    Add grafics and select day
    Add the grafic for the reports with the gem chartkick and also add the
    select day.

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,5 +1,15 @@
 class ReportsController < ApplicationController
   def index
+    if params[:start_date].present?
+      @start_date = Date.parse(params[:start_date])
+    else
+      @start_date = Date.today
+    end
+    @data = Item.where("created_at >= ?", @start_date.beginning_of_day).where("created_at <= ?", @start_date.end_of_day).group_by_day(:created_at).count
+  end
+  
+
+  def generate
   end
 
   def new
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,29 +7,30 @@
 #   Character.create(name: "Luke", movie: movies.first)
 
 # Tables
-tables = Table.create([{ name: "Mesa A", size: 5 }, 
-                      { name: "Mesa B", size: 2 },
-                      { name: "Mesa C", size: 4 },
-                      { name: "Mesa D", size: 6 },
-                      { name: "Mesa E", size: 8 }])
+# tables = Table.create([{ name: "Mesa A", size: 5 }, 
+#                       { name: "Mesa B", size: 2 },
+#                       { name: "Mesa C", size: 4 },
+#                       { name: "Mesa D", size: 6 },
+#                       { name: "Mesa E", size: 8 }])
 
-#Items 
-items = Item.create([{ name: "Hamburguesa", price: 70, category: "Comida", image: "URL" }, 
-                    { name: "Sprite", price: 15, category: "Bebida", image: "URL" },
-                    { name: "Pay de queso", price: 45, category: "Postre", image: "URL" },
-                    { name: "Pizza", price: 120, category: "Comida", image: "URL" },
-                    { name: "Cafe", price: 30, category: "Bebida", image: "URL" }])
+#Items
+Item.create(name: "Te", price: 15, category: "Bebida", image: "URL")
+# items = Item.create([{ name: "Hamburguesa", price: 70, category: "Comida", image: "URL" }, 
+#                     { name: "Sprite", price: 15, category: "Bebida", image: "URL" },
+#                     { name: "Pay de queso", price: 45, category: "Postre", image: "URL" },
+#                     { name: "Pizza", price: 120, category: "Comida", image: "URL" },
+#                     { name: "Cafe", price: 30, category: "Bebida", image: "URL" }])
 
 #Orders
-orders = Order.create([{ tables_id: tables.first.id, status: "In progress", total: nil, note: "Hamburguesa sin pepinillos" }, 
-                      { tables_id: tables.second.id, status: "In progress", total: nil, note: "Pizza con champiñones" },
-                      { tables_id: tables.third.id, status: "In progress", total: nil, note: "Cafe sin azucar" },
-                      { tables_id: tables.fourth.id, status: "In progress", total: nil, note: "Hamburguesa sin verdura y con captsun" },
-                      { tables_id: tables.fifth.id, status: "In progress", total: nil, note: "Traer el refresco cuando este la comida" }])
+# orders = Order.create([{ tables_id: tables.first.id, status: "In progress", total: nil, note: "Hamburguesa sin pepinillos" }, 
+#                       { tables_id: tables.second.id, status: "In progress", total: nil, note: "Pizza con champiñones" },
+#                       { tables_id: tables.third.id, status: "In progress", total: nil, note: "Cafe sin azucar" },
+#                       { tables_id: tables.fourth.id, status: "In progress", total: nil, note: "Hamburguesa sin verdura y con captsun" },
+#                       { tables_id: tables.fifth.id, status: "In progress", total: nil, note: "Traer el refresco cuando este la comida" }])
 
-#Ordersitems
-OrdersItem.create(orders_id: orders.first.id , items_id: items.second.id)
-OrdersItem.create(orders_id: orders.third.id , items_id: items.second.id)
-OrdersItem.create(orders_id: orders.fifth.id , items_id: items.fourth.id)
-OrdersItem.create(orders_id: orders.second.id , items_id: items.first.id)
-OrdersItem.create(orders_id: orders.fourth.id , items_id: items.fifth.id)
+# #Ordersitems
+# OrdersItem.create(orders_id: orders.first.id , items_id: items.second.id)
+# OrdersItem.create(orders_id: orders.third.id , items_id: items.second.id)
+# OrdersItem.create(orders_id: orders.fifth.id , items_id: items.fourth.id)
+# OrdersItem.create(orders_id: orders.second.id , items_id: items.first.id)
+# OrdersItem.create(orders_id: orders.fourth.id , items_id: items.fifth.id)

0643885f9eb32738ff840bebcdee6916c3fc775f
Arnoldo Magaña <elswordmon@gmail.com>
2023-02-27 20:48:37 -0600

    edit

--- app/javascript/image.js
+++
@@ -1,6 +0,0 @@
-function actualizarInputFile() {
-    var filename = "'" + this.value.replace(/^.*[\\\/]/, '') + "'";
-    this.parentElement.style.setProperty('--fn', filename);
-    }
-    
-    document.querySelectorAll(".file-select input").forEach((ele)=>ele.addEventListener('change', actualizarInputFile));

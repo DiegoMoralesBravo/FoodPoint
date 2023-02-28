8d4a2759b8e489dca9f21dadeea9e353674cf763
Arnoldo Magaña <elswordmon@gmail.com>
2023-02-28 15:33:06 -0600

    edits in seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -31,76 +31,72 @@ require "open-uri"
 
 User.create({:email => "admin@gmail.com", :password => "123456", :password_confirmation => "123456" })
 
-#numberTables = 10
+numberTables = 10
 
-#numberitemsfood = 10
-#numberitemsdessert = 20
-#numberitemsdrink = 5
-#totalnumberitems = numberitemsfood + numberitemsdessert + numberitemsdrink
+numberitemsfood = 10
+numberitemsdessert = 8
+numberitemsdrink = 5
+totalnumberitems = numberitemsfood + numberitemsdessert + numberitemsdrink
 
-#numberOrders = numberTables
+numberOrders = numberTables
 
 # Tables
-#(numberTables).times do |i|
-#  Table.create(
-#    name: Faker::Ancient.unique.god, 
-#    size: Faker::Number.between(from: 2, to: 10))
-#end
+(numberTables).times do |i|
+  Table.create(
+    name: Faker::Ancient.unique.god, 
+    size: Faker::Number.between(from: 2, to: 10))
+end
 
-#Items Food
-#10.times do
-#  item = Item.create(
-#    name: Faker::Food.unique.dish, 
-#    price: Faker::Number.between(from: 50, to: 200), 
-#    category: 'Food')
-#  item.image.attach(
-#    io:  File.open(File.join(Rails.root,'/app/assets/images/pasta.jpg')),
-#    filename: 'pasta.jpg')
-#  end
-
-#Items Dessert
-#numberitemsdessert.times do |i|
-#  Item.create(
-#    name: Faker::Dessert.unique.variety, 
-#    price: Faker::Number.between(from: 20, to: 100), 
-#    category: 'dessert')
-#end
-
-#Items drinks
-#numberitemsdrink.times do |i|
-#  Item.create(
-#    name: Faker::Beer.unique.brand, 
-#    price: Faker::Number.between(from: 1, to: 50), 
-#    category: 'drink')
-#end
-
-#numberOrders.times do |i|
-#  Order.create(
-#    tables_id: i+1,
-#    status: Faker::Boolean.boolean(true_ratio: 0.5),
-#    total: Faker::Number.between(from: 300, to: 1000),
-#    note: Faker::Food.description
-#  )
-#end
+numberOrders.times do |i|
+  Order.create(
+    tables_id: i+1,
+    status: Faker::Boolean.boolean(true_ratio: 0.5),
+    total: Faker::Number.between(from: 300, to: 1000),
+    note: Faker::Food.description
+  )
+end
 
 #Ordersitems
+numberOrders.times do |i|
+  howmanyitems = Faker::Number.between(from: 3, to: 6)
+  howmanyitems.times do |j|
+    itemID = Faker::Number.between(from: 1, to: totalnumberitems)
+    OrdersItem.create(orders_id: i+1  , items_id: itemID)
+  end
+end 
 
-#numberOrders.times do |i|
-#  howmanyitems = Faker::Number.between(from: 3, to: 6)
-#  howmanyitems.times do |j|
-#    itemID = Faker::Number.between(from: 1, to: totalnumberitems)
-#    OrdersItem.create(orders_id: i+1  , items_id: itemID)
-#  end
-#end 
+#food
+numberitemsfood.times do
+  item = Item.create(
+    name: Faker::Food.unique.dish, 
+    price: Faker::Number.between(from: 50, to: 200), 
+    category: 'Food')
+  item.image.attach(
+    io: File.open('app/assets/images/pasta.jpg'),
+    filename: 'p.jpg'
+  )
+end
 
-5.times do
+#dessert
+numberitemsdessert.times do
   item = Item.create(
     name: Faker::Dessert.unique.variety,
     price: Faker::Number.between(from: 20, to: 100),
-    category: 'dessert'
-    )
+    category: 'dessert')
   item.image.attach(
-    io: File.open('app/assets/images/pasta.jpg'),
+    io: File.open('app/assets/images/caramel-topped-ice-cream.jpg'),
+    filename: 'p.jpg'
+  )
+end
+
+#Items drinks
+numberitemsdrink.times do
+  item = Item.create(
+    name: Faker::Beer.unique.brand, 
+    price: Faker::Number.between(from: 1, to: 50), 
+    category: 'drink')
+  item.image.attach(
+    io: File.open('app/assets/images/beer.jpg'),
     filename: 'p.jpg'
   )
 end

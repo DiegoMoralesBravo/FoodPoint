7284795308b63f6ce597e8240a36f408082f6817
DiegoMoralesBravo <diegomorales1359@gmail.com>
2023-03-07 10:56:09 -0600

    Remove button added

--- app/javascript/service.js
+++ app/javascript/service.js
@@ -10,8 +10,8 @@ function addEvent() {
     items[i].onclick = function () {
       const item = this;
       const name = item.childNodes[3].childNodes[1].innerText;
+      const category = item.childNodes[5].childNodes[3].childNodes[1].childNodes[1].src
       const price = 200;
-      // console.log(item.childNodes[1]);
 
       const listItem = document.createElement("li");
       const divIcon = document.createElement("div");
@@ -22,12 +22,15 @@ function addEvent() {
       const deleteImg = document.createElement('img');
       const priceItem = document.createElement("p");
 
-      deleteImg.src = '/assets/delete_white_24dp.svg';
-      deleteButton.append(deleteImg);
-
       listItem.className = 'item'
       divIcon.className = 'list-icon'
       divPrice.className = 'list-price'
+
+      imgItem.src = category;
+
+      deleteImg.src = '/assets/delete_white_24dp.svg';
+      deleteButton.append(deleteImg);
+
 
       nameItem.textContent = name;
       priceItem.textContent = '$' + price;
@@ -38,7 +41,10 @@ function addEvent() {
       listItem.append(divIcon, divPrice);
 
       listItems.append(listItem);
-    };
 
+      deleteButton.onclick = () => {
+        listItem.remove()
+      }
+    };
   }
 }

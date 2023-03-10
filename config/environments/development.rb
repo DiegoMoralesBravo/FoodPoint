c7d85e7096eb28259b89de101d9112c18d54617e
MaxRetana <maxretana981@gmail.com>
2023-03-09 21:31:33 -0600

    Add new host

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -11,6 +11,9 @@ Rails.application.configure do
   # Do not eager load code on boot.
   config.eager_load = false
 
+  # Host
+  config.hosts << 'foodpoint.up.railway.app'
+
   # Show full error reports.
   config.consider_all_requests_local = true
 

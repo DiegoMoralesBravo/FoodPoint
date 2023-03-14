b2ee34e57197ecd1373b2b1cf5f28416cb0ea138
ikerismak <ikerismak@gmail.com>
2023-03-13 18:29:46 -0600

    search and controller feature added

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,7 +2,17 @@ class OrdersController < ApplicationController
 
   def index
     @items = Item.all
-    @order = Order.count
+
+    @input = params[:name]
+    if @input
+      @products = Item.where("name ILIKE ?", "%#{@input}%");
+      @order = Order.count
+      # render json: @products
+    else
+      @products = Item.all
+      @order = Order.count
+    end
+
   end
 
 end

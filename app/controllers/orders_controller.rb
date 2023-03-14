5561c629a103c70f033e0c9819da46c4979af228
ikerismak <ikerismak@gmail.com>
2023-03-13 19:13:28 -0600

    feat: button and search field added and working
    this feature allows search all items on the menu by name

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,18 +1,15 @@
 class OrdersController < ApplicationController
-
   def index
     @items = Item.all
 
     @input = params[:name]
     if @input
-      @products = Item.where("name ILIKE ?", "%#{@input}%");
+      @products = Item.where('name ILIKE ?', "%#{@input}%")
       @order = Order.count
       # render json: @products
     else
       @products = Item.all
       @order = Order.count
     end
-
   end
-
 end

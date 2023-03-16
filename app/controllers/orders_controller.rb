7bb956d332cd9216ba5821d43d8620f81b681cd4
DiegoMoralesBravo <diegomorales1359@gmail.com>
2023-03-15 18:36:04 -0600

    Object for new order created and received in controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,18 +1,30 @@
 class OrdersController < ApplicationController
+  skip_before_action :verify_authenticity_token
   def index
     @items = Item.all
     @category = params[:category]
     @input = params[:name]
-    if @input
-      @products = Item.where('name ILIKE ?', "%#{@input}%")
-      @order = Order.count
-      # render json: @products
-    elsif @category.present?
-      @products = Item.where('category ILIKE ?', "%#{@category}%")
-      @order = Order.count
-    else
-      @products = Item.all
-      @order = Order.count
+    @products = if @input
+                  Item.where('name ILIKE ?', "%#{@input}%")
+                # render json: @products
+                elsif @category.present?
+                  Item.where('category ILIKE ?', "%#{@category}%")
+                else
+                  Item.all
+                end
+    @order = Order.count
+  end
+
+  def create
+    newOrder = params[:_json]
+    puts @order
+    newOrder.each do |item|
+      OrdersItem.create(orders_id: 1, items_id: 1, quantity: 1)
+      # if @order.save
+      #   puts 'GUARDADO'
+      # else
+      #   putes 'NO GUARDADO'
+      # end
     end
   end
 end
--- app/javascript/service.js
+++ app/javascript/service.js
@@ -4,16 +4,20 @@ function addEvent() {
   const totalItems = document.querySelector(".total-items");
   const totalPrice = document.querySelector(".total-price");
   const cancelButton = document.querySelector(".cancel-order");
+  const sendButton = document.querySelector(".send-to-kitchen");
+  const csrfToken = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
+  const csrfParam = document.querySelector('meta[name="csrf-param"]').getAttribute('content');
 
   let button;
   let sumItems = 0;
   let sumPrice = 0;
   let newOrder = []
-  
+
   for (let i = 0; i < items.length; i++) {
     button = items[i].querySelector(".button-add");
 
     button.onclick = function () {
+      let item = {}
       const name = items[i].childNodes[3].childNodes[1].innerText;
       const category = items[i].querySelector(".icon_category").src
       const price = items[i].querySelector(".price").innerText;
@@ -21,7 +25,7 @@ function addEvent() {
       sumPrice += parseInt(price);
       sumItems += 1;
       totalItems.innerText = sumItems;
-      totalPrice.innerText =  sumPrice;
+      totalPrice.innerText = sumPrice;
 
       const listItem = document.createElement("li");
       const divIcon = document.createElement("div");
@@ -52,21 +56,71 @@ function addEvent() {
 
       listItems.append(listItem);
 
+      item = {
+        name,
+        quantity: 1
+      }
+
+      newOrder = itemCheck(item, newOrder, 'add')
+
       deleteButton.onclick = () => {
         sumPrice -= price;
         sumItems -= 1;
         totalItems.innerText = sumItems;
-        totalPrice.innerText =  sumPrice;
+        totalPrice.innerText = sumPrice;
         listItem.remove()
+        newOrder = itemCheck(item, newOrder, 'delete')
       }
-    };
+    }
   }
 
   cancelButton.onclick = function () {
     totalItems.innerText = 0;
-    totalPrice.innerText =  0;
+    totalPrice.innerText = 0;
     listItems.innerHTML = '';
+    newOrder = [];
   }
+
+  sendButton.onclick = function () {
+    console.log('Click')
+    fetch('/orders', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        [csrfParam]: csrfToken
+      },
+      body: JSON.stringify(newOrder)
+    })
+  }
+}
+
+function itemCheck(item, order, mode) {
+  let i;
+  let flag = false;
+  for (i = 0; i < order.length; i++) {
+    if (order[i].name === item.name) {
+      item = order[i]
+      flag = true;
+      break;
+    }
+  }
+
+  if (flag) {
+    if (mode === 'add') {
+      item.quantity += 1;
+    }
+    if (mode === 'delete') {
+      item.quantity -= 1;
+      if (item.quantity === 0) {
+        order = order.splice(i, i);
+      }
+    }
+  } else {
+    if (mode === 'add') {
+      order.push(item);
+    }
+  }
+  return order
 }
 
 addEvent();

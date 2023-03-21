185a99e3ee153fbb3cbd654b55b8f53a988bed57
DiegoMoralesBravo <diegomorales1359@gmail.com>
2023-03-21 12:16:03 -0600

    Selection of table after creation of a new order

--- app/javascript/navbar.js
+++ app/javascript/navbar.js
@@ -1,10 +1,10 @@
-const openNav = () => {
+var openNav = () => {
   document.getElementById("mySidenav").style.width = "10rem";
   document.getElementById("main").style.marginLeft = "10rem";
   document.getElementById("logo").style.display = "none";
 }
 
-const closeNav = () => {
+var closeNav = () => {
   document.getElementById("mySidenav").style.width = "0";
   document.getElementById("main").style.marginLeft= "0";
   document.getElementById("logo").style.display = "block";

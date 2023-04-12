1c31c61aae083bc4dd8eb493afce93d52a73a30e
Diego Morales <diegomorales1359@gmail.com>
2023-04-11 21:34:33 -0600

    Inventory view was added

--- app/controllers/items_planner_controller.rb
+++ app/controllers/items_planner_controller.rb
@@ -1,4 +1,6 @@
 class ItemsPlannerController < ApplicationController
+  before_action :authenticate_user!
+
   def index
     @ingredients = Ingredient.all
   end
--- app/javascript/inventory.js
+++ app/javascript/inventory.js
@@ -1,27 +1,70 @@
 function inventory() {
   const ingredients = document.querySelectorAll(".ingredient-inventory");
+  const csrfToken = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
+  const csrfParam = document.querySelector('meta[name="csrf-param"]').getAttribute('content');
   for (let i = 0; i < ingredients.length; i++) {
     let ingredient = ingredients[i];
-    button = ingredient.querySelector(".edit-inventory");
+    let btnConfirm = ingredient.querySelector('.confirm-button');
+    let btnCancel = ingredient.querySelector('.cancel-button')
+    let button = ingredient.querySelector(".delete-button");
+    const totalContainer = ingredient.querySelector(".ingredient-total");
+    const value = totalContainer.innerText;
+
     button.onclick = function () {
-      const totalContailer = ingredient.querySelector(".ingredient-total");
       const totalValue = ingredient.querySelector(".value-total");
       const input = document.createElement("input");
-      const confirm = document.createElement("button");
-      const cancel = document.createElement("button");
 
       input.setAttribute("type", "text");
-      input.setAttribute("value", totalValue.innerText == 'Sin registro' ? 0 : totalValue.innerText);
-      input.style.width = "20%"
-      confirm.className = 'confirm-inv'
-      cancel.className = 'cancel-inv'
-      cancel.innerText = "X";
-      confirm.innerText = "✔"
+      input.setAttribute("value", totalValue.innerText == 'Sin registro' ? 0 : totalValue.value);
+      input.style.width = "40%"
+      input.className = "input-total";
 
-      totalContailer.replaceChild(input, totalValue);
-      totalContailer.appendChild(confirm);
-      totalContailer.appendChild(cancel);
+      totalContainer.replaceChild(input, totalValue);
+      this.style.display = "none";
+      btnCancel.style.display = "inline";
+      btnConfirm.style.display = "inline";
     }
+
+    btnCancel.onclick = function () {
+      updateValue(ingredient, totalContainer, value)
+      hideButtons(btnConfirm, btnCancel, button);
+    }
+
+    btnConfirm.onclick = function () {
+      const idIngredient = ingredient.querySelector('.ingredient-id').innerText;
+      const input = ingredient.querySelector(".input-total");
+      const valueInput = input.value;
+
+      updateValue(ingredient, totalContainer, valueInput)
+      hideButtons(btnConfirm, btnCancel, button);
+      sendInventory({ ingredient: idIngredient, total: valueInput });
+    }
+
+  }
+
+  function hideButtons(btnConfirm, btnCancel, button) {
+    btnConfirm.style.display = "none";
+    btnCancel.style.display = "none";
+    button.style.display = "block";
+  }
+
+  function updateValue(ingredient, totalContainer, value) {
+    const input = ingredient.querySelector(".input-total");
+    const span = document.createElement("span");
+
+    span.className = "value-total";
+    span.innerText = value;
+    totalContainer.replaceChild(span, input);
+  }
+  function sendInventory(data) {
+    fetch('/inventory', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        [csrfParam]: csrfToken
+      },
+      body: JSON.stringify(data)
+    })
   }
 }
 

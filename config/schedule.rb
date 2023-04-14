0ae880495ec760c528d1b024dfba71dd8744f190
Arnoldo Magaña <elswordmon@gmail.com>
2023-04-14 14:25:15 -0600

    update status at end of day

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,27 +1,6 @@
-# Use this file to easily define all of your cron jobs.
-#
-# It's helpful, but not entirely necessary to understand cron before proceeding.
-# http://en.wikipedia.org/wiki/Cron
+env :PATH, ENV['PATH']
+set :output, 'log/cron.log'
 
-# Example:
-#
-# set :output, "/path/to/my/cron_log.log"
-#
-# every 2.hours do
-#   command "/usr/bin/some_great_command"
-#   runner "MyModel.some_method"
-#   rake "some:great:rake:task"
-# end
-#
-# every 4.days do
-#   runner "AnotherModel.prune_old_records"
-# end
-
-# Learn more: http://github.com/javan/whenever
-
-
-every 1.day, at: '11:59 pm' do
-    runner "Order.where(status: 'progress').update_all(status: 'paid')"
-    runner "Order.where(status: 'wait').update_all(status: 'paid')"
-    runner "Order.where(status: 'done').update_all(status: 'paid')"
+every 1.days, at: "11:59 pm" do
+    rake "status:update"
 end

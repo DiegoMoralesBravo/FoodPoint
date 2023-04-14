26d1aae1e97dfdda5befd4b137d864540f113d98
ikerismak <ikerismak@gmail.com>
2023-04-14 11:45:35 -0600

    feat: Show item image and description in sidebar when clicked, add main ingredients table
    Implemented functionality to show the item's image and description in the sidebar when it's clicked, as well as a table of main ingredients for the item.
    
    Changes made include:
    - Created a new JavaScript function to handle item clicks and display information in the sidebar
    - Modified the HTML and CSS to accommodate the new JavaScript function and display the item information
    - Added a new table to display the main ingredients for the item, and populated it with data from the database
    - Modified the server-side code to retrieve the item's description and image from the database and send it to the client for display
    
    This improves the user experience by providing more detailed information about the item when it's clicked, including an image and description. It also provides a table of main ingredients for the item, making it easier for the user to see what's included in the dish.
    
    BREAKING CHANGE: None.

--- app/javascript/planner.js
+++ app/javascript/planner.js
@@ -3,28 +3,40 @@ function planner() {
   let itemName = document.querySelector(".planner-item-name");
   let itemCategory = document.querySelector(".planner-item-category");
   let itemPrice = document.querySelector(".planner-item-price");
+  let itemDescription = document.querySelector(".item-info-description");
+  let itemImage = document.querySelector(".item-planner-image-container");
+
+
+  console.log(itemImage);
 
 
 
 
-  console.log(itemList);
+
 
   itemList.forEach((item) => {
     let button = item.querySelector(".button-planner-edit");
     let name = item.querySelector(".items-planner-name").innerHTML;
     let category = item.querySelector(".items-planner-category").innerHTML;
     let price = item.querySelector(".items-planner-price").innerHTML;
+    let description = item.querySelector(".items-planner-description").innerHTML
+    let image = item.querySelector(".items-planner-image").innerHTML;
 
-    console.log(price);
+
 
 
     button.addEventListener("click", (e) => {
       console.log(name);
       console.log(price);
+      console.log(image);
 
       itemName.innerHTML = name;
       itemCategory.innerHTML = category;
       itemPrice.innerHTML = price;
+      itemDescription.innerHTML = description;
+      itemImage.innerHTML = image;
+
+
 
     });
   });

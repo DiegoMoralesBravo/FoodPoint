82b48aa47d493e71c6cc7237eb86631a6970bfa3
ikerismak <ikerismak@gmail.com>
2023-04-13 21:36:55 -0600

    feat: Show item information according to selected row using JavaScript, retrieve image from database for display when product is selected
    Implemented functionality to show item information dynamically when a row is selected in the table, using JavaScript to avoid page reloading and make the interaction more dynamic. Additionally, the item's image is retrieved from the database so that it's available to display when the product is selected.
    
    Changes made include:
    - Created a new JavaScript function to handle item selection and display information dynamically
    - Modified the HTML and CSS to accommodate the new JavaScript function and display the item information
    - Modified the server-side code to retrieve the item's image from the database and send it to the client for display
    
    This improves the user experience by making the interaction with the table more dynamic and avoiding page reloading when selecting an item. It also provides a better presentation of the items by displaying the image when the product is selected.
    
    BREAKING CHANGE: None.

--- app/javascript/planner.js
+++ app/javascript/planner.js
@@ -1,6 +1,10 @@
 function planner() {
   let itemList = document.querySelectorAll(".items-row");
   let itemName = document.querySelector(".planner-item-name");
+  let itemCategory = document.querySelector(".planner-item-category");
+  let itemPrice = document.querySelector(".planner-item-price");
+
+
 
 
   console.log(itemList);
@@ -8,11 +12,20 @@ function planner() {
   itemList.forEach((item) => {
     let button = item.querySelector(".button-planner-edit");
     let name = item.querySelector(".items-planner-name").innerHTML;
+    let category = item.querySelector(".items-planner-category").innerHTML;
+    let price = item.querySelector(".items-planner-price").innerHTML;
+
+    console.log(price);
+
 
     button.addEventListener("click", (e) => {
       console.log(name);
+      console.log(price);
 
       itemName.innerHTML = name;
+      itemCategory.innerHTML = category;
+      itemPrice.innerHTML = price;
+
     });
   });
 }
